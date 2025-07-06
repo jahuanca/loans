010e2e37ac389ed0bb4e6a587b45703746625faa
jahuanca <41663809+jahuanca@users.noreply.github.com>
2025-07-06 03:05:09 -0500

    feat: warn of outstanding fees

--- src/quota/controllers/pay_quota.js
+++ src/quota/controllers/pay_quota.js
@@ -3,14 +3,14 @@ const payQuotaUseCaseExecute = require("../use_cases/pay_quota_use_case")
 
 const payQuotaController = async (req, res) => {
     const {
-        idOfQuota: id,
-        paidDate: date,
+        id_of_quota,
+        paid_date,
     } = req.body
-    const [err, quota] = await getPromise(payQuotaUseCaseExecute({
-        id,
-        date,
-    }))
+    const [err, quota] = await getPromise(
+        payQuotaUseCaseExecute({ id_of_quota, paid_date })
+    )
     if (err) return res.status(500).json({message: err.message})
+    if (quota == null) return res.status(404).json({message: 'No se pudo editar la quota'})
     return res.status(200).json(quota)
 }
 
